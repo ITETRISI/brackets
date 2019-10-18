8f094b796913a3c2a11e56120d8e7f3b59b3f995
Daniil Asipenka <ocipenkoden@gmail.com>
2019-10-18 19:08:51 +0300

    feat: complete brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let re ;
+    for(let i in str){
+    for (let value of bracketsConfig) {
+        !isNaN(value[0]) ? re = new RegExp(`${value[0]}${value[1]}`,"g") : re = new RegExp(`\\${value[0]}\\${value[1]}`,"g");
+        str = str.replace(re, '')
+    }
+    if (str.length==0)
+        return true;
+    } 
+    if (str.length!=0){
+        return false
+    } else
+    return true
 }
